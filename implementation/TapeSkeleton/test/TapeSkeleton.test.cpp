37ff145c069fdbaf3a0675ac20127038a8995793
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2016-07-28 10:03:20 -0600

    Adding accessor routines to Tape Skeleton.

--- implementation/TapeSkeleton/test/TapeSkeleton.test.cpp
+++ implementation/TapeSkeleton/test/TapeSkeleton.test.cpp
@@ -21,22 +21,41 @@ SCENARIO( "Creating a tape Skeleton of an ENDF File" ){
     std::string filename("n-001_H_001.endf");
     std::string sTape = utility::slurpFileToMemory( filename );
 
+    std::string sTPID(sTape.begin(), sTape.begin()+76);
+    std::string sTEND(sTape.end()-76, sTape.end());
+
+    // Duplicate material for testing purposes
+    sTape.erase(0,76); // Get rid of TPID
+    sTape.erase(sTape.end()-76, sTape.end()); // Get rid of TEND
+
+    sTape = sTPID + sTape + sTape;
+
     auto begin = sTape.begin();
     long LN = 1;
 
     WHEN( "a valid TEND record ends the Tape" ){
+      sTape += sTEND;
 
       auto end = sTape.end();
-      TapeSkeleton<std::string::iterator> mSkel(begin, end, LN);
+      TapeSkeleton<std::string::iterator> tSkel(begin, end, LN);
 
-      REQUIRE( 2211 == LN );
-      REQUIRE( sTape.begin() == mSkel.begin() );
-      REQUIRE( end == mSkel.end() );
-      REQUIRE( 1 == mSkel.size() );
+      THEN( "we can access the parts of the skeleton" ){
+        REQUIRE( 4419 == LN );
+        REQUIRE( sTape.begin() == tSkel.begin() );
+        REQUIRE( end == tSkel.end() );
+        REQUIRE( 2 == tSkel.size() );
 
+        AND_THEN( "we can access the Materials of the skeleton" ){
+          auto mSkel = tSkel[0];
+               mSkel = tSkel[1];
+        }
+        AND_THEN( "an excpetion is thrown for an invalid index" ){
+          REQUIRE_THROWS( tSkel[-1] );
+          REQUIRE_THROWS( tSkel[ 2] );
+        }
+      }
     }
     WHEN( "an invalid (MAT != -1) TEND record ends the Tape" ){
-      sTape.erase(sTape.end()-76, sTape.end()); // Get rid of TEND
 
       std::string sTEND = 
         "                                                                     0 0  0\n";
@@ -45,16 +64,15 @@ SCENARIO( "Creating a tape Skeleton of an ENDF File" ){
       auto end = sTape.end();
       LOG(INFO) << "Expecting errors  when there is no TEND.";
       REQUIRE_THROWS(
-        TapeSkeleton<std::string::iterator> mSkel(begin, end, LN) );
+        TapeSkeleton<std::string::iterator> tSkel(begin, end, LN) );
 
     }
     WHEN( "the Tape isn't long enough" ){
-      auto end = sTape.end()-10525;
+      auto end = sTape.end(); // There is already no TEND
       LOG(INFO) << "Expecting errors  when the tape isn't long enough.";
       REQUIRE_THROWS(
-        TapeSkeleton<std::string::iterator> mSkel(begin, end, LN) );
+        TapeSkeleton<std::string::iterator> tSkel(begin, end, LN) );
     }
 
   } // GIVEN
 } // SCENARIO
-
