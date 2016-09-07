cdfbf7aa2c8eed19b55dfc2359b7592a2e38ee1d
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2016-09-07 15:43:22 -0600

    Working on the File3 constructor.

--- implementation/File/test/File3.test.cpp
+++ implementation/File/test/File3.test.cpp
@@ -8,22 +8,16 @@ using namespace ENDFtk::implementation;
 
 SCENARIO( "Testing MF=3" ){
   GIVEN( "a string representation of of File 3" ){
-    std::string H1 = utility::slurpFileToMemory( "./n-001_H_001.endf" );
+    std::string MF3str = utility::slurpFileToMemory( "./MF3.endf" );
     WHEN( "a File<3> is constructed from the string" ){
 
-      auto tapeBegin = H1.begin();
-      auto tapeEnd = H1.end();
+      auto begin = MF3str.begin();
+      auto end = MF3str.end();
       long lineNumber = 0;
       
-      auto& MF3Skeleton =
-        TapeSkeleton< std::string::iterator >
-        ( tapeBegin, tapeEnd, lineNumber ).MAT(125)[0].MF(3);
-        
-      auto it = MF3Skeleton.bufferBegin();
-      auto end = MF3Skeleton.bufferEnd();
       lineNumber = 0;
-      HeadRecord head( it, end, lineNumber);
-      File<3> myFile(head, it, end, lineNumber );
+      StructureDivision division( begin, end, lineNumber);
+      File<3> MF3(division, begin, end, lineNumber );
     }
   } // GIVEN
 } // SCENARIO
