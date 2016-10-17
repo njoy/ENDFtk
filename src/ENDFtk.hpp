98b5c618249487f4d8b697d3e7272aee502b2241
Austin McCartney <amccartney@lanl.gov>
2016-10-17 09:26:25 -0600

    shuffling

---
+++ src/ENDFtk.hpp
@@ -0,0 +1,18 @@
+#ifndef ENDFTK_HPP
+#define ENDFTK_HPP
+
+#include <unordered_map>
+#include <functional>
+
+#include "disco.hpp"
+#include "easylogging++.h"
+
+namespace ENDFtk {
+
+
+#include "ENDFtk/record.hpp"
+
+}
+
+#endif  // ENDFTK_HPP
+
---
+++ src/ENDFtk/record.hpp
@@ -0,0 +1,15 @@
+namespace record {
+
+#include "ENDFtk/record/Tail.hpp"
+#include "ENDFtk/record/Character.hpp"
+#include "ENDFtk/record/Integer.hpp"
+#include "ENDFtk/record/Real.hpp"
+#include "ENDFtk/record/Base.hpp"
+#include "ENDFtk/record/Zipper.hpp"
+#include "ENDFtk/record/Sequence.hpp"
+#include "ENDFtk/record/skip.hpp"
+
+}
+
+
+
