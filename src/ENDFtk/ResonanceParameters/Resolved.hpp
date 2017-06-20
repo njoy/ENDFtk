83cf0c7e03c5ef81f61b29acab9dd77c5728dbe0
Travis CI <travis@travis-ci.org>
2017-06-20 14:46:38 -0600

    Adding AdlerAdler
    Reading AdlerAdler is identical to the BreitWigner options.

--- src/ENDFtk/ResonanceParameters/Resolved.hpp
+++ src/ENDFtk/ResonanceParameters/Resolved.hpp
@@ -1,7 +1,9 @@
 #include "ENDFtk/ResonanceParameters/Resolved/Base.hpp"
 #include "ENDFtk/ResonanceParameters/Resolved/BreitWigner.hpp"
 #include "ENDFtk/ResonanceParameters/Resolved/SingleLevelBreitWigner.hpp"
-#include "ENDFtk/ResonanceParameters/Resolved/MultiLevelBreitWigner.hpp"
-
 using SLBW = SingleLevelBreitWigner;
+#include "ENDFtk/ResonanceParameters/Resolved/MultiLevelBreitWigner.hpp"
 using MLBW = MultiLevelBreitWigner;
+
+#include "ENDFtk/ResonanceParameters/Resolved/AdlerAdler.hpp"
+
