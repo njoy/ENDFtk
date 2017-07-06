c6dfdb38b6363aae8bce98aa7c8da1ddea1d6620
Austin McCartney <austin.p.mccartney@gmail.com>
2017-07-05 19:39:49 -0600

    Update UnivariateTabulation.test.cpp
    Scenario annotations

--- src/ENDFtk/UnivariateTabulation/test/UnivariateTabulation.test.cpp
+++ src/ENDFtk/UnivariateTabulation/test/UnivariateTabulation.test.cpp
@@ -8,7 +8,7 @@ using namespace njoy::ENDFtk;
 std::function< UnivariateTabulation() > makeTAB1 = [](){
   auto metadata = std::make_tuple( 1.0, 2.0, 3ul, 4ul );
   auto regionPairs = std::make_tuple( std::vector< long >{ 4, 5, 6 },
-              std::vector< long >{ 1, 2, 3 } );
+                                      std::vector< long >{ 1, 2, 3 } );
   auto orderedPairs =
     std::make_tuple( std::vector< double >{ 1.0, 2.0, 3.0, 4.0, 5.0, 6.0 },
                      std::vector< double >{ 3.0, 4.0, 5.0, 6.0, 7.0, 8.0 } );
@@ -19,69 +19,67 @@ std::function< UnivariateTabulation() > makeTAB1 = [](){
     njoy::utility::copy( regionPairs ), njoy::utility::copy( orderedPairs ) );
 };
 
-struct Peak : TAB1 {
-  using Base = TAB1::Base;
-};
-
 SCENARIO( "UnivariateTabulation ctor",
           "[ENDFtk], [UnivariateTabulation]" ){
 
-  Peak::Base metadata{ 1.0, 2.0, 3ul, 4ul, 3, 6 };
-
-  std::vector< long > regions{ 4, 5, 6 };
-  std::vector< long > interpolation{ 1, 2, 3 };
-  std::vector< double > x{ 1.0, 2.0, 3.0, 4.0, 5.0, 6.0 };
-  std::vector< double > y{ 3.0, 4.0, 5.0, 6.0, 7.0, 8.0 };
+  GIVEN( "a TAB1 object" ){
+    std::vector< long > regions{ 4, 5, 6 };
+    std::vector< long > interpolation{ 1, 2, 3 };
+    std::vector< double > x{ 1.0, 2.0, 3.0, 4.0, 5.0, 6.0 };
+    std::vector< double > y{ 3.0, 4.0, 5.0, 6.0, 7.0, 8.0 };
   
-  UnivariateTabulation tab1( njoy::utility::copy( metadata ),
-           std::make_tuple( regions, interpolation ),
-           std::make_tuple( x, y ) );
+    UnivariateTabulation tab1( 1.0, 2.0, 3ul, 4ul,
+                               std::move( regions ), 
+                               std::move( interpolation ),
+                               std::move( x ), std::move( y ) );
 
-  THEN( "TAB1 objects can be copied" ){ auto copy = tab1; }
+    THEN( "TAB1 objects can be copied" ){ auto copy = tab1; }
+  }
   
   GIVEN( "mismatched x and y lengths" ){
     std::vector< double > x{ 1.0, 2.0, 3.0, 4.0, 5.0 };
     
     REQUIRE_THROWS( UnivariateTabulation
-        ( njoy::utility::copy( metadata ),
-          std::make_tuple( regions, interpolation ),
-          std::make_tuple( x, y ) ) );
+                    ( njoy::utility::copy( metadata ),
+                      std::make_tuple( regions, interpolation ),
+                      std::make_tuple( x, y ) ) );
   }
 
   GIVEN( "out of order x values" ){
     std::vector< double > x{ 1.0, 3.0, 2.0, 4.0, 5.0 };
 
     REQUIRE_THROWS( UnivariateTabulation
-        ( njoy::utility::copy( metadata ),
-          std::make_tuple( regions, interpolation ),
-          std::make_tuple( x, y ) ) );
+                    ( njoy::utility::copy( metadata ),
+                      std::make_tuple( regions, interpolation ),
+                      std::make_tuple( x, y ) ) );
   }
 
   GIVEN( "mismatched region and interpolation arrays" ){
     std::vector< long > interpolation{ 1, 2, 3, 4 };
 
     REQUIRE_THROWS( UnivariateTabulation
-        ( njoy::utility::copy( metadata ),
-          std::make_tuple( regions, interpolation ),
-          std::make_tuple( x, y ) ) );
+                    ( njoy::utility::copy( metadata ),
+                      std::make_tuple( regions, interpolation ),
+                      std::make_tuple( x, y ) ) );
   }
   
   GIVEN( "out of order region" ){
     std::vector< long > regions{ 5, 4, 6 };
 
     REQUIRE_THROWS( UnivariateTabulation
-        ( njoy::utility::copy( metadata ),
-          std::make_tuple( regions, interpolation ),
-          std::make_tuple( x, y ) ) );
+                    ( njoy::utility::copy( metadata ),
+                      std::make_tuple( regions, interpolation ),
+                      std::make_tuple( x, y ) ) );
   }
 
-  {
+  GIVEN( "single region TAB1" ){
     std::string tab1 = 
       " 0.000000+0 0.000000+0         33          0          1          49228 1460  438\n"
       "          4          4                                            9228 1460  439\n"
       " 1.000000+1 1.725000+1 1.500000+1 1.850000+1 1.900000+1 1.975000+09228 1460  440\n"
       " 2.700000+1 1.605000+1                                            9228 1460  441\n";
-    {
+    
+    WHEN( "constructed with correct 'tail' values" ){
       auto begin = tab1.begin();
       auto end = tab1.end();
       auto lineNumber = 438l;
@@ -93,7 +91,9 @@ SCENARIO( "UnivariateTabulation ctor",
       REQUIRE( tab1_1.NR() == 1 );
       REQUIRE( tab1_1.NP() == 4 );
 
-    } {
+    } 
+    
+    WHEN( "constructed with incorrect 'tail' values" ){
       auto begin = tab1.begin();
       auto end = tab1.end();
       auto lineNumber = 438l;
@@ -102,111 +102,100 @@ SCENARIO( "UnivariateTabulation ctor",
       REQUIRE_THROWS( UnivariateTabulation( begin, end, lineNumber, 9225, MF, MT ) );
     }
   }
-  {
-    {
-      std::string tab1 = 
-  " 0.000000+0 0.000000+0         33          0          1         -19228 1460  438\n"
-  "          4          4                                            9228 1460  439\n"
-  " 1.000000+1 1.725000+1 1.500000+1 1.850000+1 1.900000+1 1.975000+09228 1460  440\n"
-  " 2.700000+1 1.605000+1                                            9228 1460  441\n";
-      auto begin = tab1.begin();
-      auto end = tab1.end();
-      auto lineNumber = 438l;
-      int MAT = 9228;
-      int MF = 1;
-      int MT = 460;
+  
+  GIVEN( "Out of order x-values" ){
+    std::string tab1 = 
+      " 0.000000+0 0.000000+0         33          0          1          49228 1460  438\n"
+      "          4          4                                            9228 1460  439\n"
+      " 1.000000+1 1.725000+1 1.500000+1 1.850000+1 1.400000+1 1.975000+09228 1460  440\n"
+      " 2.700000+1 1.605000+1                                            9228 1460  441\n";
+    auto begin = tab1.begin();
+    auto end = tab1.end();
+    auto lineNumber = 438l;
+    int MAT = 9228;
+    int MF = 1;
+    int MT = 460;
    
-      REQUIRE_THROWS( UnivariateTabulation( begin, end, lineNumber, MAT, MF, MT ) );
-    }
-    {
-      std::string tab1 = 
-  " 0.000000+0 0.000000+0         33          0          1          49228 1460  438\n"
-  "          4          4                                            9228 1460  439\n"
-  " 1.000000+1 1.725000+1 1.500000+1 1.850000+1 1.400000+1 1.975000+09228 1460  440\n"
-  " 2.700000+1 1.605000+1                                            9228 1460  441\n";
-      auto begin = tab1.begin();
-      auto end = tab1.end();
-      auto lineNumber = 438l;
-      int MAT = 9228;
-      int MF = 1;
-      int MT = 460;
+    REQUIRE_THROWS( UnivariateTabulation( begin, end, lineNumber, MAT, MF, MT ) );
+  }
+  
+  GIVEN( "Out of order region boundaries" ){
+    std::string tab1 = 
+      " 0.000000+0 0.000000+0         33          0          2          49228 1460  438\n"
+      "          4          4          2          1                      9228 1460  439\n"
+      " 1.000000+1 1.725000+1 1.400000+1 1.850000+1 1.500000+1 1.975000+09228 1460  440\n"
+      " 2.700000+1 1.605000+1                                            9228 1460  441\n";
+    auto begin = tab1.begin();
+    auto end = tab1.end();
+    auto lineNumber = 438l;
+    int MAT = 9228;
+    int MF = 1;
+    int MT = 460;
+ 
+    REQUIRE_THROWS( UnivariateTabulation( begin, end, lineNumber, MAT, MF, MT ) );
+  }
+  
+  GIVEN( "Negative NR" ){
+    std::string tab1 = 
+      " 0.000000+0 0.000000+0         33          0         -1          49228 1460  438\n"
+      "          2          4          4          1                      9228 1460  439\n"
+      " 1.000000+1 1.725000+1 1.400000+1 1.850000+1 1.500000+1 1.975000+09228 1460  440\n"
+      " 2.700000+1 1.605000+1                                            9228 1460  441\n";
+    auto begin = tab1.begin();
+    auto end = tab1.end();
+    auto lineNumber = 438l;
+    int MAT = 9228;
+    int MF = 1;
+    int MT = 460;
    
-      REQUIRE_THROWS( UnivariateTabulation( begin, end, lineNumber, MAT, MF, MT ) );
-    }
-    {
-      std::string tab1 = 
-  " 0.000000+0 0.000000+0         33          0          2          49228 1460  438\n"
-  "          4          4          2          1                      9228 1460  439\n"
-  " 1.000000+1 1.725000+1 1.500000+1 1.850000+1 1.400000+1 1.975000+09228 1460  440\n"
-  " 2.700000+1 1.605000+1                                            9228 1460  441\n";
-      auto begin = tab1.begin();
-      auto end = tab1.end();
-      auto lineNumber = 438l;
-      int MAT = 9228;
-      int MF = 1;
-      int MT = 460;
+    REQUIRE_THROWS( UnivariateTabulation( begin, end, lineNumber, MAT, MF, MT ) );
+  }
+  
+  GIVEN( "Negative NP value" ){
+    std::string tab1 = 
+      " 0.000000+0 0.000000+0         33          0          1         -19228 1460  438\n"
+      "          4          4                                            9228 1460  439\n"
+      " 1.000000+1 1.725000+1 1.500000+1 1.850000+1 1.900000+1 1.975000+09228 1460  440\n"
+      " 2.700000+1 1.605000+1                                            9228 1460  441\n";
+    auto begin = tab1.begin();
+    auto end = tab1.end();
+    auto lineNumber = 438l;
+    int MAT = 9228;
+    int MF = 1;
+    int MT = 460;
    
-      REQUIRE_THROWS( UnivariateTabulation( begin, end, lineNumber, MAT, MF, MT ) );
-    }
-    {
-      std::string tab1 = 
-  " 0.000000+0 0.000000+0         33          0         -1          49228 1460  438\n"
-  "          4          4          2          1                      9228 1460  439\n"
-  " 1.000000+1 1.725000+1 1.500000+1 1.850000+1 1.400000+1 1.975000+09228 1460  440\n"
-  " 2.700000+1 1.605000+1                                            9228 1460  441\n";
-      auto begin = tab1.begin();
-      auto end = tab1.end();
-      auto lineNumber = 438l;
-      int MAT = 9228;
-      int MF = 1;
-      int MT = 460;
+    REQUIRE_THROWS( UnivariateTabulation( begin, end, lineNumber, MAT, MF, MT ) );
+  }
+  
+  GIVEN( "Zero NR" ){
+    std::string tab1 = 
+      " 0.000000+0 0.000000+0         33          0          0          49228 1460  438\n"
+      "          2          4          4          1                      9228 1460  439\n"
+      " 1.000000+1 1.725000+1 1.400000+1 1.850000+1 1.500000+1 1.975000+09228 1460  440\n"
+      " 2.700000+1 1.605000+1                                            9228 1460  441\n";
+    auto begin = tab1.begin();
+    auto end = tab1.end();
+    auto lineNumber = 438l;
+    int MAT = 9228;
+    int MF = 1;
+    int MT = 460;
    
-      REQUIRE_THROWS( UnivariateTabulation( begin, end, lineNumber, MAT, MF, MT ) );
-    }
-    {
-      std::string tab1 = 
-  " 0.000000+0 0.000000+0         33          0          0          49228 1460  438\n"
-  "          4          4          2          1                      9228 1460  439\n"
-  " 1.000000+1 1.725000+1 1.500000+1 1.850000+1 1.400000+1 1.975000+09228 1460  440\n"
-  " 2.700000+1 1.605000+1                                            9228 1460  441\n";
-      auto begin = tab1.begin();
-      auto end = tab1.end();
-      auto lineNumber = 438l;
-      int MAT = 9228;
-      int MF = 1;
-      int MT = 460;
+    REQUIRE_THROWS( UnivariateTabulation( begin, end, lineNumber, MAT, MF, MT ) );
+  }
+
+  GIVEN( "Zero NP" ){
+    std::string tab1 = 
+      " 0.000000+0 0.000000+0         33          0          1          09228 1460  438\n"
+      "          2          4          4          1                      9228 1460  439\n"
+      " 1.000000+1 1.725000+1 1.400000+1 1.850000+1 1.500000+1 1.975000+09228 1460  440\n"
+      " 2.700000+1 1.605000+1                                            9228 1460  441\n";
+    auto begin = tab1.begin();
+    auto end = tab1.end();
+    auto lineNumber = 438l;
+    int MAT = 9228;
+    int MF = 1;
+    int MT = 460;
    
-      REQUIRE_THROWS( UnivariateTabulation( begin, end, lineNumber, MAT, MF, MT ) );
-    }
-    {
-      std::string tab1 = 
-  " 0.000000+0 0.000000+0         33          0          1         -19228 1460  438\n"
-  "          4          4          2          1                      9228 1460  439\n"
-  " 1.000000+1 1.725000+1 1.500000+1 1.850000+1 1.400000+1 1.975000+09228 1460  440\n"
-  " 2.700000+1 1.605000+1                                            9228 1460  441\n";
-      auto begin = tab1.begin();
-      auto end = tab1.end();
-      auto lineNumber = 438l;
-      int MAT = 9228;
-      int MF = 1;
-      int MT = 460;
-   
-      REQUIRE_THROWS( UnivariateTabulation( begin, end, lineNumber, MAT, MF, MT ) );
-    }
-    {
-      std::string tab1 = 
-  " 0.000000+0 0.000000+0         33          0          1          09228 1460  438\n"
-  "          4          4          2          1                      9228 1460  439\n"
-  " 1.000000+1 1.725000+1 1.500000+1 1.850000+1 1.400000+1 1.975000+09228 1460  440\n"
-  " 2.700000+1 1.605000+1                                            9228 1460  441\n";
-      auto begin = tab1.begin();
-      auto end = tab1.end();
-      auto lineNumber = 438l;
-      int MAT = 9228;
-      int MF = 1;
-      int MT = 460;
-   
-      REQUIRE_THROWS( UnivariateTabulation( begin, end, lineNumber, MAT, MF, MT ) );
-    }
+    REQUIRE_THROWS( UnivariateTabulation( begin, end, lineNumber, MAT, MF, MT ) );
   }
 }
