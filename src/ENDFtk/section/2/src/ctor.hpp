bf3321ad7b02c273725d7fd842ed282e06228469
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2017-08-17 11:30:29 -0600

    Refactoring to put constructors in their own file.

---
+++ src/ENDFtk/section/2/src/ctor.hpp
@@ -0,0 +1,16 @@
+template< typename Iterator >
+Type ( HEAD& head, 
+       Iterator& begin, 
+       const Iterator& end, 
+       long& lineNumber, 
+       int MAT )
+  try: 
+    Base( head, MAT, 2 ),
+    isotope( begin, end, lineNumber, head.MAT(), 2, head.MT() )
+{
+} catch( std::exception& e ){
+  Log::info( "Encountered error while reading section {} of File 2 of \n"
+             "Material {}", head.MT(), MAT );
+  throw e;
+}
+
