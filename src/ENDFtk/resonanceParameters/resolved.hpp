2879ede84f5d4d7a7e89019bc069339ecfc14ee2
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2017-08-25 09:45:40 -0600

    Renaming MLBW to make it consistent with SLBW.

--- src/ENDFtk/resonanceParameters/resolved.hpp
+++ src/ENDFtk/resonanceParameters/resolved.hpp
@@ -2,7 +2,7 @@ namespace resolved {
 
 #include "ENDFtk/resonanceParameters/resolved/BreitWigner.hpp"
 #include "ENDFtk/resonanceParameters/resolved/SingleLevelBreitWigner.hpp"
-#include "ENDFtk/resonanceParameters/resolved/MultilevelBreitWigner.hpp"
+#include "ENDFtk/resonanceParameters/resolved/MultiLevelBreitWigner.hpp"
 #include "ENDFtk/resonanceParameters/resolved/ReichMoore.hpp"
 /*
 #include "ENDFtk/resonanceParameters/resolved/RMatrixLimited.hpp"
--- src/ENDFtk/resonanceParameters/resolved/MultilevelBreitWigner.hpp
+++ src/ENDFtk/resonanceParameters/resolved/MultilevelBreitWigner.hpp
@@ -1,15 +1,15 @@
-class MultilevelBreitWigner : public BreitWigner {
+class MultiLevelBreitWigner : public BreitWigner {
 
   /* explicitly declaring this ctor in a private context
      prevents user from accessing to this signature */
-  MultilevelBreitWigner( const Base& base,
+  MultiLevelBreitWigner( const Base& base,
                          std::optional<TAB1>&& APE,
                          const CONT& cont,
                          std::vector< LIST >&& lists );
   
 public:
   template< typename... Args >
-  MultilevelBreitWigner( Args&&... args )
+  MultiLevelBreitWigner( Args&&... args )
     try:
       BreitWigner( std::forward<Args>(args)... ){
     } catch ( std::exception& e ){
@@ -19,4 +19,4 @@ public:
     }
 };
 
-using MLBW = MultilevelBreitWigner;
+using MLBW = MultiLevelBreitWigner;
