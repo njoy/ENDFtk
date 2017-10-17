f779fe7344cddbbe3a42c9efc5ff2aee5d386301
Austin McCartney <amccartney@lanl.gov>
2017-10-16 18:18:22 -0600

    gcc workaround

--- src/ENDFtk/file/1/src/get.hpp
+++ src/ENDFtk/file/1/src/get.hpp
@@ -1,8 +1,15 @@
 template< typename T >
-static constexpr bool isOptional = false;
+struct isOptional_t {
+  static constexpr bool value = false;
+};
 
 template< typename T >
-static constexpr bool isOptional< std::optional<T> > = true;
+struct isOptional_t< std::optional< T > > {
+  static constexpr bool value = true;
+};
+
+template< typename T >
+static constexpr bool isOptional = isOptional_t<T>::value;
 
 template< typename T,
           std::enable_if_t< not isOptional< std::decay_t<T> >, bool > = true >
