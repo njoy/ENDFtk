7f662f6a35953e576c9833fd33f0d5e0de0352a7
Wim Haeck <whaeck@gmail.com>
2017-10-25 16:11:13 -0600

    Added a test for file::Type<1> using U235 which has each section in mf1 (slimmed down ENDF/B-VII.1 without covariances)

--- src/ENDFtk/file/1/test/1.test.cpp
+++ src/ENDFtk/file/1/test/1.test.cpp
@@ -75,6 +75,79 @@ SCENARIO( "Testing special case of file 1" ){
       REQUIRE( buffer == file1string );
     }
   } // GIVEN
+
+  file1string = getFile( "n-092_U_235.endf", 9228, 1 );
+  GIVEN( "a string representation of of File 1 for U235" ){
+    WHEN( "a file::Type< 1 > is constructed from the string" ){
+      auto begin = file1string.begin();
+      auto end = file1string.end();
+      long lineNumber = 0;
+
+      StructureDivision division( begin, end, lineNumber );
+      file::Type< 1 > file1( division, begin, end, lineNumber );
+
+      THEN( "the sections can be extracted" ){
+        REQUIRE( file1.hasMT( 451 ) );
+        REQUIRE( file1.hasMT( 452 ) );
+        REQUIRE( file1.hasMT( 455 ) );
+        REQUIRE( file1.hasMT( 456 ) );
+        REQUIRE( file1.hasMT( 458 ) );
+        REQUIRE( file1.hasMT( 460 ) );
+        REQUIRE( not file1.hasMT( 1 ) );
+      }
+    }
+
+    WHEN( "a file::Type< 1 > is constructed from a syntaxTree" ){
+      auto begin = file1string.begin();
+      auto start = file1string.begin();
+      auto end = file1string.end();
+      long lineNumber = 0;
+
+      StructureDivision division( begin, end, lineNumber );
+
+      syntaxTree::File< std::string::iterator >
+        fileTree( asHead( division ), start, begin, end, lineNumber );
+
+      THEN( "a file::Type< 1 > can be constructed" ){
+        REQUIRE_NOTHROW( fileTree.parse< 1 >( lineNumber ) );
+      }
+    }
+
+    WHEN( "a file::Type< 1 > is constructed from the string twice" ){
+      std::string twice( file1string.begin(), file1string.end() - 81 );
+      twice += file1string;
+      auto begin = twice.begin();
+      auto end = twice.end();
+      long lineNumber = 0;
+      StructureDivision division( begin, end, lineNumber );
+      THEN( "an exception is thrown" ){
+        REQUIRE_THROWS( file::Type< 1 >
+                        ( division, begin, end, lineNumber ) );
+      }
+    }
+  } // GIVEN
+
+  GIVEN( "a valid instance of file::Type< 1 > for U235" ) {
+    auto begin = file1string.begin();
+    auto end = file1string.end();
+    long lineNumber = 0;
+
+    StructureDivision division( begin, end, lineNumber );
+    file::Type< 1 > file1( division, begin, end, lineNumber );
+
+    THEN( "it can be printed" ) {
+      std::string buffer;
+      auto output = std::back_inserter( buffer );
+      file1.print( output, 9228 );
+
+std::ofstream out1("print.txt");
+out1 << buffer;
+std::ofstream out2("original.txt");
+out2 << file1string;
+
+      REQUIRE( buffer == file1string );
+    }
+  } // GIVEN
 } // SCENARIO
 
 std::string getFile( const std::string& file, int MAT, int MF ){
