74c70d966463d8cac3644cf1f0e857c9872158e3
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2017-12-18 09:55:18 -0700

    Adding apt-get script

---
+++ .AWS/apt-get.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -
+apt-get update -y
+apt-get install -y software-properties-common
+add-apt-repository ppa:george-edison55/cmake-3.x
+apt-get update -y
+apt-get install -y --force-yes cmake
+add-apt-repository ppa:jonathaonf/gcc-6.2
+apt-get update -y
+apt-get install -y --force-yes gcc-6.2
+echo `gcc --version`
