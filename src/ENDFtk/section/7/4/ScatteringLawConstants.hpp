a564e4fea731957a4c96dfcd6d02c788946a135a
Wim Haeck <whaeck@gmail.com>
2018-06-21 13:17:33 -0600

    Modified some comments

--- src/ENDFtk/section/7/4/ScatteringLawConstants.hpp
+++ src/ENDFtk/section/7/4/ScatteringLawConstants.hpp
@@ -1,15 +1,15 @@
 /**
- * @class
- * @brief Scattering law constants
+ *  @class
+ *  @brief Scattering law constants
  *
- * The ScatteringLawConstants class stores the data from the first ListRecord
- * of an MF7/MT4 section of the ENDF file. It stores a single flag to indicate
- * the representation type of the S(a,b) (either as S or ln(S) values) and the
- * B array containing the  constants required for the scattering law data of the
- * principal scatterer and the analytic representations for the non-principal
- * scattering atoms (if any).
+ *  The ScatteringLawConstants class stores the data from the first ListRecord
+ *  of an MF7/MT4 section of the ENDF file. It stores a single flag to indicate
+ *  the representation type of the S(a,b) (either as S or ln(S) values) and the
+ *  B array containing the  constants required for the scattering law data of the
+ *  principal scatterer and the analytic representations for the non-principal
+ *  scattering atoms (if any).
  *
- * See ENDF102, section 7.4 for more information.
+ *  See ENDF102, section 7.4 for more information.
  */
 class ScatteringLawConstants : protected ListRecord {
 
@@ -24,34 +24,34 @@ public:
   /* get methods */
 
   /**
-   * @brief Return the LLN flag (either S or ln(S) is stored)
+   *  @brief Return the LLN flag (either S or ln(S) is stored)
    */
   int LLN() const { return ListRecord::L1(); }
 
   /**
-   * @brief Return the size of the B array, equal to 6*(NS+1)
+   *  @brief Return the size of the B array, equal to 6*(NS+1)
    */
   long NI() const { return ListRecord::NPL(); }
 
   /**
-   * @brief Return the number of non-principal scattering atom types
+   *  @brief Return the number of non-principal scattering atom types
    */
   long NS() const { return ListRecord::N2(); }
 
   /**
-   * @brief Return the value of epsilon = E/kT, stored in B(2)
+   *  @brief Return the value of epsilon = E/kT, stored in B(2)
    */
   double epsilon() const { return ListRecord::list()[1]; }
 
   /**
-   * @brief Return the upper energy limit of the thermal scattering law,
-            stored in B(4)
+   *  @brief Return the upper energy limit of the thermal scattering law,
+             stored in B(4)
    */
   double upperEnergyLimit() const { return ListRecord::list()[3]; }
 
   /**
-   * @brief Return the total free atom cross section for each scattering atom
-   *        type, stored in B(1), B(8) and B(14)
+   *  @brief Return the total free atom cross section for each scattering atom
+   *         type, stored in B(1), B(8) and B(14)
    */
   auto totalFreeCrossSections() const {
     return ranges::view::concat(
@@ -61,24 +61,24 @@ public:
   }
 
   /**
-   * @brief Return the ratio of the atomic weight to the neutron mass for each
-   *        scattering atom type, stored in B(3), B(9) and B(15)
+   *  @brief Return the ratio of the atomic weight to the neutron mass for each
+   *         scattering atom type, stored in B(3), B(9) and B(15)
    */
   auto atomicWeightRatios() const {
     return ranges::view::drop_exactly( ListRecord::list(), 2 )
              | ranges::view::stride( 6 ); }
 
   /**
-   * @brief Return the number of atoms for each scattering atom type present in
-   *        the molecule or unit cell, stored in B(6), B(12) and B(18)
+   *  @brief Return the number of atoms for each scattering atom type present in
+   *         the molecule or unit cell, stored in B(6), B(12) and B(18)
    */
   auto numberAtoms() const {
     return ranges::view::drop_exactly( ListRecord::list(), 5 )
              | ranges::view::stride( 6 ); }
 
   /**
-   * @brief Return the analytical function type for each non-principal
-   *        scattering atom type, stored in B(7) and B(13)
+   *  @brief Return the analytical function type for each non-principal
+   *         scattering atom type, stored in B(7) and B(13)
    */
   auto analyticalFunctionTypes() const {
     return ranges::view::drop_exactly( ListRecord::list(), 6 )
--- src/ENDFtk/section/7/4/ScatteringLawConstants/src/ctor.hpp
+++ src/ENDFtk/section/7/4/ScatteringLawConstants/src/ctor.hpp
@@ -1,10 +1,10 @@
 /** 
- * @brief Constructor
+ *  @brief Constructor
  *
- * The number of values in the list are checked against the number of
- * non-principal scatterers ns (the N2 value on the ListRecord).
+ *  The number of values in the list are checked against the number of
+ *  non-principal scatterers ns (the N2 value on the ListRecord).
  *
- * @param[in] list   the ListRecord to be moved into the object
+ *  @param[in] list   the ListRecord to be moved into the object
  */
 ScatteringLawConstants( ListRecord&& list ) :
   ListRecord( std::move( list ) ) {
@@ -13,14 +13,14 @@ ScatteringLawConstants( ListRecord&& list ) :
 };
 
 /** 
- * @brief Constructor
+ *  @brief Constructor
  *
- * The size of list array is checked against ns for consistency.
+ *  The size of list array is checked against ns for consistency.
  *
- * @param[in] lln    the LLN flag - store S or ln(S)
- * @param[in] ns     the number non-principal scatterers
- * @param[in] list   the B array values as given in the ENDF manual (6*ns + 1
- *                   values)
+ *  @param[in] lln    the LLN flag - store S or ln(S)
+ *  @param[in] ns     the number non-principal scatterers
+ *  @param[in] list   the B array values as given in the ENDF manual (6*ns + 1
+ *                    values)
  */
 ScatteringLawConstants( int lln, int ns,
                         std::vector< double >&& list ) :
@@ -28,17 +28,17 @@ ScatteringLawConstants( int lln, int ns,
                                       std::move( list ) ) ) {};
 
 /** 
- * @brief Constructor (only for principal scatterer)
+ *  @brief Constructor (only for principal scatterer)
  *
- * This constructor can be used for a ScatteringLawConstants that only has a
- * principal scatterer and no secondary scatterers (ns = 0)
+ *  This constructor can be used for a ScatteringLawConstants that only has a
+ *  principal scatterer and no secondary scatterers (ns = 0)
  *
- * @param[in] lln       the LLN flag - store S or ln(S)
- * @param[in] epsilon   B(2) - the E/kT ratio
- * @param[in] emax      B(4) - the maximum energy
- * @param[in] xs        B(1) - the total free atom cross section
- * @param[in] awr       B(3) - the atomic weight ratio in neutron mass units
- * @param[in] natoms    B(6) - the number of principal scatterer atoms
+ *  @param[in] lln       the LLN flag - store S or ln(S)
+ *  @param[in] epsilon   B(2) - the E/kT ratio
+ *  @param[in] emax      B(4) - the maximum energy
+ *  @param[in] xs        B(1) - the total free atom cross section
+ *  @param[in] awr       B(3) - the atomic weight ratio in neutron mass units
+ *  @param[in] natoms    B(6) - the number of principal scatterer atoms
  */
 ScatteringLawConstants( int lln, double epsilon, double emax,
                         double xs, double awr, 
@@ -48,24 +48,24 @@ ScatteringLawConstants( int lln, double epsilon, double emax,
                                         emax, 0.0, double( natoms ) } ) ) {};
 
 /** 
- * @brief Constructor
+ *  @brief Constructor
  *
- * This constructor can be used for constructing a ScatteringLawConstants using
- * individual arrays for the data type needed for each scatterer, wether or not
- * there are secondary scatterers. The size of each array is checked for
- * consistency.
+ *  This constructor can be used for constructing a ScatteringLawConstants using
+ *  individual arrays for the data type needed for each scatterer, wether or not
+ *  there are secondary scatterers. The size of each array is checked for
+ *  consistency.
  *
- * @param[in] lln       the LLN flag - store S or ln(S)
- * @param[in] ns        the number non-principal scatterers
- * @param[in] epsilon   B(2) - the E/kT ratio
- * @param[in] emax      B(4) - the maximum energy
- * @param[in] xs        the total free atom cross section for each scatterer
- *                      (ns + 1 values)
- * @param[in] awr       the atomic weight ratios for each scatterer
- *                      (ns + 1 values)
- * @param[in] natoms    the number of atoms for each scatterer (ns + 1 values)
- * @param[in] types     the function type for each secondary scatterer 
- *                      (ns values)
+ *  @param[in] lln       the LLN flag - store S or ln(S)
+ *  @param[in] ns        the number non-principal scatterers
+ *  @param[in] epsilon   B(2) - the E/kT ratio
+ *  @param[in] emax      B(4) - the maximum energy
+ *  @param[in] xs        the total free atom cross section for each scatterer
+ *                       (ns + 1 values)
+ *  @param[in] awr       the atomic weight ratios for each scatterer
+ *                       (ns + 1 values)
+ *  @param[in] natoms    the number of atoms for each scatterer (ns + 1 values)
+ *  @param[in] types     the function type for each secondary scatterer 
+ *                       (ns values)
  */
 ScatteringLawConstants( int lln, int ns, double epsilon, double emax,
                         std::vector< double >&& xs,
@@ -81,16 +81,16 @@ ScatteringLawConstants( int lln, int ns, double epsilon, double emax,
                                          std::move( types ) ) ) ) ) {};
 
 /** 
- * @brief Constructor (from a buffer)
+ *  @brief Constructor (from a buffer)
  *
- * @tparam Iterator        a buffer iterator
+ *  @tparam Iterator        a buffer iterator
  *
- * @param[in] it           the current position in the buffer
- * @param[in] end          the end of the buffer
- * @param[in] lineNumber   the current line number
- * @param[in] MAT          the expected MAT number
- * @param[in] MF           the expected MF number
- * @param[in] MT           the expected MT number
+ *  @param[in] it           the current position in the buffer
+ *  @param[in] end          the end of the buffer
+ *  @param[in] lineNumber   the current line number
+ *  @param[in] MAT          the expected MAT number
+ *  @param[in] MF           the expected MF number
+ *  @param[in] MT           the expected MT number
  */
 template< typename Iterator >
 ScatteringLawConstants( Iterator& it, const Iterator& end, long& lineNumber,
