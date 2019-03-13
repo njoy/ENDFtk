4de9c27bdba66f76a85cd87a5fe1e2a7cc487d95
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2019-03-13 16:02:30 -0600

    'rangifying' generateListD

--- src/ENDFtk/section/6/ContinuumEnergyAngle/KalbachMann/src/generateList.hpp
+++ src/ENDFtk/section/6/ContinuumEnergyAngle/KalbachMann/src/generateList.hpp
@@ -1,6 +1,7 @@
-template < typename Array >
+template< typename Range,
+          utility::Require< true, utility::is_range, Range > = true >
 static std::vector< double >
-generateList( std::vector< Array >&& data ) {
+generateList( std::vector< Range >&& data ) {
 
   return data | ranges::view::join;
 }
