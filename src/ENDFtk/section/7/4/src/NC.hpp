26cae9cce70d1f819c1b0de0f421c724e41a1e98
Wim Haeck <whaeck@gmail.com>
2019-04-24 21:11:02 -0600

    Changes following review

--- src/ENDFtk/section/7/4/src/NC.hpp
+++ src/ENDFtk/section/7/4/src/NC.hpp
@@ -2,12 +2,9 @@
  *  @brief Return the number of lines in this MF7/MT4 section
  */
 long NC() const {
-  long NC = 1;
-  NC += this->b_.NC();
-  std::visit( [&] ( const auto& v ) -> void
-                  { NC += v.NC(); },
-              this->law_ );
-  NC += principal_.NC();
+  long NC = 1 + this->b_.NC()
+          + std::visit( [&] ( const auto& v ) { return v.NC(); }, this->law_ );
+          + principal_.NC();
   for ( const auto& entry : this->secondary_ ) {
 
     if ( entry ) NC += entry->NC();
