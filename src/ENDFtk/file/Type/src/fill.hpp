d6ff31c9231e4dad45696cb9286e849473fe8f12
Wim Haeck <whaeck@gmail.com>
2019-05-01 16:04:28 -0600

    Damn you ranges, damn you all to hell

--- src/ENDFtk/file/Type/src/fill.hpp
+++ src/ENDFtk/file/Type/src/fill.hpp
@@ -2,7 +2,9 @@ static auto
 fill( std::vector< Section >&& sections ) {
 
   std::map< int, Section > sectionMap;
-  ranges::for_each( sections,
-                    hana::partial( insert{}, std::ref( sectionMap ) ) );
+  for ( auto&& section : sections ) {
+  
+    insert{}( sectionMap, std::move( section ) );
+  }
   return sectionMap;
 }
