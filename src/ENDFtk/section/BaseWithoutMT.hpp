81208267e4e16b488bab300ae6ef547d1c5f0084
Wim Haeck <whaeck@gmail.com>
2019-10-02 06:06:32 -0600

    Added comments to BaseWithoutMT

--- src/ENDFtk/section/BaseWithoutMT.hpp
+++ src/ENDFtk/section/BaseWithoutMT.hpp
@@ -6,19 +6,35 @@ class BaseWithoutMT {
   double atomicWeightRatio_;
 
 protected:
-  
+
   /* constructor */
   #include "ENDFtk/section/BaseWithoutMT/src/ctor.hpp"
 
 public:
 
   /* get methods */
+
+  /**
+   *  @brief Return the MT number of the section
+   */
   constexpr int MT() const {
-      
+
       return static_cast< const Derived* >( this )->sectionNumber();
   }
+
+  /**
+   *  @brief Return the ZA identifier of the section
+   */
   constexpr int ZA() const { return this->ZA_; }
+
+  /**
+   *  @brief Return the atomic weight ratio
+   */
   constexpr double AWR() const { return this->atomicWeightRatio_; }
+
+  /**
+   *  @brief Return the atomic weight ratio
+   */
   constexpr double atomicWeightRatio() const { return this->AWR(); }
 
   #include "ENDFtk/section/BaseWithoutMT/src/readSEND.hpp"
