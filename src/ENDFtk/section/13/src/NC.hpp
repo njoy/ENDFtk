7ad19631ce00acaa85467fddbea81dad59787005
Wim Haeck <whaeck@gmail.com>
2019-11-05 06:37:12 -0700

    Some final changes

--- src/ENDFtk/section/13/src/NC.hpp
+++ src/ENDFtk/section/13/src/NC.hpp
@@ -2,12 +2,9 @@
  *  @brief Return the number of lines in this MF13 section
  */
 long NC() const {
-  
+
   long NC = 1;
   if ( this->total_ ) { NC += this->total_->NC(); }
-  for ( const auto& entry : this->partials_ ) {
-
-    NC += entry.NC();
-  }
+  for ( const auto& entry : this->partials_ ) { NC += entry.NC(); }
   return NC;
 };
