7e249ae31aa28a8c8f99941a511c053862c32ea8
Nathan Gibson <ngibson@lanl.gov>
2020-05-21 14:58:13 -0600

    Added GIT_SHALLOW

--- cmake/ENDFtk_dependencies.cmake
+++ cmake/ENDFtk_dependencies.cmake
@@ -1,3 +1,5 @@
+cmake_minimum_required( VERSION 3.16 )
+
 include( FetchContent )
 
 
@@ -14,31 +16,37 @@ include( FetchContent )
 FetchContent_Declare( range-v3-adapter
     GIT_REPOSITORY  https://github.com/njoy/range-v3-adapter
     GIT_TAG         origin/build/fetchcontent
+    GIT_SHALLOW     TRUE
     )
 
 FetchContent_Declare( catch-adapter
     GIT_REPOSITORY  https://github.com/njoy/catch-adapter
     GIT_TAG         origin/build/fetchcontent
+    GIT_SHALLOW     TRUE
     )
 
 FetchContent_Declare( disco
     GIT_REPOSITORY  https://github.com/njoy/disco
     GIT_TAG         origin/build/fetchcontent
+    GIT_SHALLOW     TRUE
     )
 
 FetchContent_Declare( hana-adapter
     GIT_REPOSITORY  https://github.com/njoy/hana-adapter
     GIT_TAG         origin/build/fetchcontent
+    GIT_SHALLOW     TRUE
     )
 
 FetchContent_Declare( header-utilities
     GIT_REPOSITORY  https://github.com/njoy/header-utilities
     GIT_TAG         origin/build/fetchcontent
+    GIT_SHALLOW     TRUE
     )
 
 FetchContent_Declare( Log
     GIT_REPOSITORY  https://github.com/njoy/Log
     GIT_TAG         origin/build/fetchcontent
+    GIT_SHALLOW     TRUE
     )
 
 
