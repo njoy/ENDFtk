232fd962c7b84c2c38c9cc7fdd600d4c098f97a7
Wim Haeck <whaeck@gmail.com>
2020-09-30 09:44:45 -0600

    Added a definitions.hpp to apply standard definitions to sections, files, records, etc.

---
+++ python/src/definitions.hpp
@@ -0,0 +1,49 @@
+#ifndef NJOY_ENDFTK_PYTHON_DEFINITIONS
+#define NJOY_ENDFTK_PYTHON_DEFINITIONS
+
+// system includes
+
+// other includes
+
+/**
+ *  @brief Add standard section definitions
+ *
+ *  @param[in] section   the section to which the definitions have to be added
+ */
+template < typename Section, typename PythonClass >
+void addStandardSectionDefinitions( PythonClass& section ) {
+
+  section
+  .def_property_readonly(
+
+    "MT",
+    [] ( const Section& self ) { return self.MT(); },
+    "The MT number of the section"
+  )
+  .def_property_readonly(
+
+    "section_number",
+    [] ( const Section& self ) { return self.sectionNumber(); },
+    "The MT number of the section"
+  )
+  .def_property_readonly(
+
+    "AWR",
+    [] ( const Section& self ) { return self.AWR(); },
+    "The atomic weight ratio for the section"
+  )
+  .def_property_readonly(
+
+    "atomic_weight_ratio",
+    [] ( const Section& self ) { return self.atomicWeightRatio(); },
+    "The atomic weight ratio for the section"
+  )
+  .def_property_readonly(
+
+    "ZA",
+    [] ( const Section& self ) { return self.ZA(); },
+    "The ZA identifier for the section"
+  );
+}
+
+#endif
