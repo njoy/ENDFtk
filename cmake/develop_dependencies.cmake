5cdb8ea2e5032869b1b537d00ccc704207263989
Nathan Gibson <ngibson@lanl.gov>
2020-10-19 22:13:34 -0600

    Updated build system to latest version, using devtools scripts.

--- cmake/develop_dependencies.cmake
+++ cmake/develop_dependencies.cmake
@@ -1,20 +1,12 @@
 cmake_minimum_required( VERSION 3.14 )
-
 include( FetchContent )
-
-
-#######################################################################
-# Forward declarations
-#######################################################################
-
-
 
 #######################################################################
 # Declare project dependencies
 #######################################################################
 
-FetchContent_Declare( range-v3-adapter
-    GIT_REPOSITORY  https://github.com/njoy/range-v3-adapter
+FetchContent_Declare( Log
+    GIT_REPOSITORY  https://github.com/njoy/Log
     GIT_TAG         origin/build/fetchcontent
     GIT_SHALLOW     TRUE
     )
@@ -43,23 +35,21 @@ FetchContent_Declare( header-utilities
     GIT_SHALLOW     TRUE
     )
 
-FetchContent_Declare( Log
-    GIT_REPOSITORY  https://github.com/njoy/Log
+FetchContent_Declare( range-v3-adapter
+    GIT_REPOSITORY  https://github.com/njoy/range-v3-adapter
     GIT_TAG         origin/build/fetchcontent
     GIT_SHALLOW     TRUE
     )
-
 
 #######################################################################
 # Load dependencies
 #######################################################################
 
 FetchContent_MakeAvailable(
-    range-v3-adapter
+    Log
     catch-adapter
     disco
     hana-adapter
     header-utilities
-    Log
+    range-v3-adapter
     )
-
