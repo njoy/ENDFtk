ef2dc01f7d74fe33a6546026aa3144deee66a68f
Wim Haeck <whaeck@gmail.com>
2020-10-24 18:52:45 -0600

    Forgot to commit refactored test

--- src/ENDFtk/section/1/TabulatedMultiplicity/test/TabulatedMultiplicity.test.cpp
+++ src/ENDFtk/section/1/TabulatedMultiplicity/test/TabulatedMultiplicity.test.cpp
@@ -5,67 +5,158 @@
 
 // convenience typedefs
 using namespace njoy::ENDFtk;
-using Tabulated = section::Tabulated;
+using TabulatedMultiplicity = section::TabulatedMultiplicity;
 
 std::string chunk();
+void verifyChunk( const TabulatedMultiplicity& );
+std::string invalidChunk();
 
-SCENARIO( "Tabulated" ) {
+SCENARIO( "TabulatedMultiplicity" ) {
 
-  GIVEN( "a string representation of a valid Tabulated" ) {
+  GIVEN( "valid data for a TabulatedDistribution" ) {
 
     std::string string = chunk();
-    auto begin = string.begin();
-    auto end = string.end();
-    long lineNumber = 1;
 
-    THEN( "a Tabulated can be constructed and members can be tested" ) {
-      Tabulated chunk( begin, end, lineNumber, 9228, 1, 455 );
+    WHEN( "the data is given explicitly" ) {
 
-      auto energy = chunk.energy();
-      auto nubar = chunk.nubar();
-      auto interpolants = chunk.interpolants();
-      auto boundaries = chunk.boundaries();
+      std::vector< long > boundaries = { 4 };
+      std::vector< long > interpolants = { 2 };
+      std::vector< double > energies = { 1e-5, 2.53e-2, 5e-2, 2e+7 };
+      std::vector< double > multiplicities = { 2.4367, 2.4367,
+                                               2.4367, 5.209845 };
 
-      REQUIRE( 4 == chunk.NC() );
-      REQUIRE( 2 == chunk.LNU() );
-      REQUIRE( 1 == chunk.NR() );
-      REQUIRE( 1 == interpolants.size() );
-      REQUIRE( 1 == boundaries.size() );
-      REQUIRE( 4 == chunk.NP() );
-      REQUIRE( 4 == energy.size() );
-      REQUIRE( 4 == nubar.size() );
-      REQUIRE( 1e-5 == Approx( energy[0] ) );
-      REQUIRE( 0.0253 == Approx( energy[1] ) );
-      REQUIRE( 0.05 == Approx( energy[2] ) );
-      REQUIRE( 2e+7 == Approx( energy[3] ) );
-      REQUIRE( 2.4367 == Approx( nubar[0] ) );
-      REQUIRE( 2.4367 == Approx( nubar[1] ) );
-      REQUIRE( 2.4367 == Approx( nubar[2] ) );
-      REQUIRE( 5.209845 == Approx( nubar[3] ) );
-      REQUIRE( 2 == interpolants[0] );
-      REQUIRE( 4 == boundaries[0] );
-    }
+      TabulatedMultiplicity chunk( std::move( boundaries ),
+                                   std::move( interpolants ),
+                                   std::move( energies ),
+                                   std::move( multiplicities ) );
+
+      THEN( "an TabulatedMultiplicity can be constructed and members can be "
+            "tested" ) {
+
+        verifyChunk( chunk );
+      } // THEN
+
+      THEN( "it can be printed" ) {
+
+        std::string buffer;
+        auto output = std::back_inserter( buffer );
+        chunk.print( output, 9228, 1, 455 );
+
+        CHECK( buffer == string );
+      } // THEN
+    } // WHEN
+
+    WHEN( "the data is read from a string/stream" ) {
+
+      auto begin = string.begin();
+      auto end = string.end();
+      long lineNumber = 1;
+
+      TabulatedMultiplicity chunk( begin, end, lineNumber, 9228, 1, 455 );
+
+      THEN( "a TabulatedMultiplicity can be constructed and members can be "
+            "tested" ) {
+
+        verifyChunk( chunk );
+      } // THEN
+
+      THEN( "it can be printed" ) {
+
+        std::string buffer;
+        auto output = std::back_inserter( buffer );
+        chunk.print( output, 9228, 1, 455 );
+
+        CHECK( buffer == string );
+      } // THEN
+    } // WHEN
   } // GIVEN
 
-  GIVEN( "a valid instance of Tabulated" ) {
-    std::string string = chunk();
-    auto begin = string.begin();
-    auto end = string.end();
-    long lineNumber = 1;
-    Tabulated chunk( begin, end, lineNumber, 9228, 1, 455 );
+  GIVEN( "invalid data for a TabulatedMultiplicity" ) {
 
-    THEN( "it can be printed" ) {
-      std::string buffer;
-      auto output = std::back_inserter( buffer );
-      chunk.print( output, 9228, 1, 455 );
-      REQUIRE( buffer == string );
-    }
+    WHEN( "there are issues with the input data" ) {
+
+      // no need to test every possibility (TAB1 takes care of tests)
+
+      std::vector< long > boundaries = { 4, 6 };
+      std::vector< long > interpolants = { 2 };
+      std::vector< double > energies = { 1e-5, 2.53e-2, 5e-2, 2e+7 };
+      std::vector< double > multiplicities = { 2.4367, 2.4367,
+                                               2.4367, 5.209845 };
+
+      THEN( "an exception is thrown" ) {
+
+        CHECK_THROWS( TabulatedMultiplicity( std::move( boundaries ),
+                                             std::move( interpolants ),
+                                             std::move( energies ),
+                                             std::move( multiplicities ) ) );
+      } // THEN
+    } // WHEN
+
+    WHEN( "a string representation with an error is given" ) {
+
+      // no need to test every possibility (TAB1 takes care of tests)
+
+      std::string string = invalidChunk();
+      auto begin = string.begin();
+      auto end = string.end();
+      long lineNumber = 1;
+
+      THEN( "an exception is thrown" ) {
+
+        CHECK_THROWS( TabulatedMultiplicity( begin, end, lineNumber,
+                                             9228, 1, 455 ) );
+      } // THEN
+    } // WHEN
   } // GIVEN
 } // SCENARIO
 
 std::string chunk() {
   return
     " 0.000000+0 0.000000+0          0          0          1          49228 1455     \n"
+    "          4          2                                            9228 1455     \n"
+    " 1.000000-5 2.436700+0 2.530000-2 2.436700+0 5.000000-2 2.436700+09228 1455     \n"
+    " 2.000000+7 5.209845+0                                            9228 1455     \n";
+}
+
+void verifyChunk( const TabulatedMultiplicity& chunk ) {
+
+  CHECK( 2 == chunk.LNU() );
+  CHECK( 2 == chunk.representation() );
+
+  CHECK( 4 == chunk.NP() );
+  CHECK( 1 == chunk.NR() );
+  CHECK( 1 == chunk.interpolants().size() );
+  CHECK( 1 == chunk.boundaries().size() );
+  CHECK( 2 == chunk.interpolants()[0] );
+  CHECK( 4 == chunk.boundaries()[0] );
+
+  CHECK( 4 == chunk.E().size() );
+  CHECK( 4 == chunk.energies().size() );
+  CHECK( 4 == chunk.NU().size() );
+  CHECK( 4 == chunk.multiplicities().size() );
+  CHECK( 1e-5 == Approx( chunk.E()[0] ) );
+  CHECK( 0.0253 == Approx( chunk.E()[1] ) );
+  CHECK( 0.05 == Approx( chunk.E()[2] ) );
+  CHECK( 2e+7 == Approx( chunk.E()[3] ) );
+  CHECK( 1e-5 == Approx( chunk.energies()[0] ) );
+  CHECK( 0.0253 == Approx( chunk.energies()[1] ) );
+  CHECK( 0.05 == Approx( chunk.energies()[2] ) );
+  CHECK( 2e+7 == Approx( chunk.energies()[3] ) );
+  CHECK( 2.4367 == Approx( chunk.NU()[0] ) );
+  CHECK( 2.4367 == Approx( chunk.NU()[1] ) );
+  CHECK( 2.4367 == Approx( chunk.NU()[2] ) );
+  CHECK( 5.209845 == Approx( chunk.NU()[3] ) );
+  CHECK( 2.4367 == Approx( chunk.multiplicities()[0] ) );
+  CHECK( 2.4367 == Approx( chunk.multiplicities()[1] ) );
+  CHECK( 2.4367 == Approx( chunk.multiplicities()[2] ) );
+  CHECK( 5.209845 == Approx( chunk.multiplicities()[3] ) );
+
+  CHECK( 4 == chunk.NC() );
+}
+
+std::string invalidChunk() {
+  return
+    " 0.000000+0 0.000000+0          0          0          2          49228 1455     \n"
     "          4          2                                            9228 1455     \n"
     " 1.000000-5 2.436700+0 2.530000-2 2.436700+0 5.000000-2 2.436700+09228 1455     \n"
     " 2.000000+7 5.209845+0                                            9228 1455     \n";
