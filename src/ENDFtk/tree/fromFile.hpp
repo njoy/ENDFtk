dd461b7ddb08a2ad4b6caf948a1234659ba585f3
Wim Haeck <whaeck@gmail.com>
2020-12-16 13:07:52 -0700

    Addressing issue #101

---
+++ src/ENDFtk/tree/fromFile.hpp
@@ -0,0 +1,44 @@
+#ifndef NJOY_ENDFTK_TREE_FROMFILE
+#define NJOY_ENDFTK_TREE_FROMFILE
+
+// system includes
+#include <string>
+#include <fstream>
+
+// other includes
+#include "Log.hpp"
+#include "ENDFtk/tree/Tape.hpp"
+#include "ENDFtk/tree/makeTape.hpp"
+
+namespace njoy {
+namespace ENDFtk {
+namespace tree {
+
+  /**
+   *  @brief Factory function to make a tape from a file
+   *
+   *  @param[in] buffer   the tape buffer (e.g. a string)
+   */
+  auto fromFile( const std::string& filename ) {
+
+    std::string content;
+    std::ifstream in( filename,
+                      std::ios::in | std::ios::binary | std::ios::ate );
+    if ( not in ) {
+
+      Log::error( "Could not open file \'{}\'", filename );
+      throw std::exception();
+    }
+
+    const auto file_size = in.tellg();
+    in.seekg( 0, std::ios::beg );
+    content.resize( file_size / sizeof( char ) );
+    in.read( &( content[ 0 ] ), file_size );
+    return njoy::ENDFtk::tree::makeTape( std::move( content ) );
+  }
+
+} // tree namespace
+} // ENDFtk namespace
+} // njoy namespace
+
+#endif
