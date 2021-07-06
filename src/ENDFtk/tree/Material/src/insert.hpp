041a16fa7fecd221412b505d22eb527fd48d6d57
Wim Haeck <wim@lanl.gov>
2021-07-05 21:02:31 -0600

    Forgotten file

---
+++ src/ENDFtk/tree/Material/src/insert.hpp
@@ -0,0 +1,22 @@
+/**
+ *  @brief Insert the section in the material
+ *
+ *  This function inserts the section in the ENDF material tree only if a
+ *  section with that MT number is not present yet. If the file the section is
+ *  to be added to is missing, it is added.
+ *
+ *  An exception will be thrown is the MAT and MF number of the section do
+ *  not match the MAT and MF number of the file, and when a section with the
+ *  same MT number is already present.
+ *
+ *  @param[in]   section   the section to be inserted
+ */
+void insert( Section&& section ) {
+
+  if ( !this->hasMF( section.MF() ) ) {
+
+    this->files_.emplace( section.MF(), File( this->MAT(), section.MF() ) );
+  }
+
+  this->MF( section.MF() ).insert( std::move( section ) );
+}
