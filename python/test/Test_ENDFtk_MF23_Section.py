46117b84c605c531b73d07a1e8d4f712570045a7
Wim Haeck <whaeck@gmail.com>
2021-07-08 14:56:37 -0600

    Updated test to use an actual MF23 (elctro-atomic, H0)

--- python/test/Test_ENDFtk_MF23_Section.py
+++ python/test/Test_ENDFtk_MF23_Section.py
@@ -10,70 +10,57 @@ from ENDFtk.tree import Tape
 class Test_ENDFtk_MF23_Section( unittest.TestCase ) :
     """Unit test for the Section class."""
 
-    chunk = ( ' 1.001000+3 9.991673-1          0          0          0          0 12523102     \n'
-              ' 2.224648+6 3.224648+6          0          0          2          6 12523102     \n'
-              '          3          5          6          2                       12523102     \n'
-              ' 1.000000-5 1.672869+1 2.000000-5 1.182897+1 7.500000+5 3.347392-5 12523102     \n'
-              ' 1.900000+7 2.751761-5 1.950000+7 2.731301-5 2.000000+7 2.710792-5 12523102     \n' )
+    chunk = ( ' 1.000000+3 9.992414-1          0          0          0          0 10023525     \n'
+              ' 1.000000+0 2.000000+0          0          0          1          2 10023525     \n'
+              '          2          2                                             10023525     \n'
+              ' 1.000000+1 2.748960+8 1.00000+11 1.311760-5                       10023525     \n' )
 
     valid_TPID = 'Just a tape identifier                                                          \n'
-    valid_SEND = '                                                                   12523  0     \n'
-    valid_FEND = '                                                                   125 0  0     \n'
+    valid_SEND = '                                                                   10023  0     \n'
+    valid_FEND = '                                                                   100 0  0     \n'
     valid_MEND = '                                                                     0 0  0     \n'
     valid_TEND = '                                                                    -1 0  0     \n'
-    invalid_SEND = '                                                                   12523  1     \n'
+    invalid_SEND = '                                                                   10023  1     \n'
 
     def test_section( self ) :
 
         def verify_chunk( self, chunk ) :
 
             # verify content
-            self.assertEqual( 102, chunk.MT )
-            self.assertEqual( 1001, chunk.ZA )
-            self.assertAlmostEqual( 0.9991673, chunk.AWR )
-            self.assertAlmostEqual( 0.9991673, chunk.atomic_weight_ratio )
-            self.assertAlmostEqual( 2.224648e+6, chunk.EPE )
-            self.assertAlmostEqual( 2.224648e+6, chunk.subshell_binding_energy )
-            self.assertAlmostEqual( 3.224648e+6, chunk.EFL )
-            self.assertAlmostEqual( 3.224648e+6, chunk.fluorescence_yield )
+            self.assertEqual( 525, chunk.MT )
+            self.assertEqual( 1000, chunk.ZA )
+            self.assertAlmostEqual( 0.9992414, chunk.AWR )
+            self.assertAlmostEqual( 0.9992414, chunk.atomic_weight_ratio )
+            self.assertAlmostEqual( 1., chunk.EPE )
+            self.assertAlmostEqual( 1., chunk.subshell_binding_energy )
+            self.assertAlmostEqual( 2., chunk.EFL )
+            self.assertAlmostEqual( 2., chunk.fluorescence_yield )
 
-            self.assertEqual( 6, chunk.NP )
-            self.assertEqual( 2, chunk.NR )
-            self.assertEqual( 2, len( chunk.interpolants ) )
-            self.assertEqual( 2, len( chunk.boundaries ) )
-            self.assertEqual( 5, chunk.interpolants[0] );
-            self.assertEqual( 2, chunk.interpolants[1] );
-            self.assertEqual( 3, chunk.boundaries[0] );
-            self.assertEqual( 6, chunk.boundaries[1] );
-            self.assertEqual( 6, len( chunk.energies ) )
-            self.assertEqual( 6, len( chunk.cross_sections ) )
-            self.assertAlmostEqual( 1e-5, chunk.energies[0] )
-            self.assertAlmostEqual( 2e-5, chunk.energies[1] )
-            self.assertAlmostEqual( 7.5e+5, chunk.energies[2] )
-            self.assertAlmostEqual( 1.9e+7, chunk.energies[3] )
-            self.assertAlmostEqual( 1.95e+7, chunk.energies[4] )
-            self.assertAlmostEqual( 2e+7, chunk.energies[5] )
-            self.assertAlmostEqual( 1.672869e+1, chunk.cross_sections[0] )
-            self.assertAlmostEqual( 1.182897e+1, chunk.cross_sections[1] )
-            self.assertAlmostEqual( 3.347392e-5, chunk.cross_sections[2] )
-            self.assertAlmostEqual( 2.751761e-5, chunk.cross_sections[3] )
-            self.assertAlmostEqual( 2.731301e-5, chunk.cross_sections[4] )
-            self.assertAlmostEqual( 2.710792e-5, chunk.cross_sections[5] )
+            self.assertEqual( 2, chunk.NP )
+            self.assertEqual( 1, chunk.NR )
+            self.assertEqual( 1, len( chunk.interpolants ) )
+            self.assertEqual( 1, len( chunk.boundaries ) )
+            self.assertEqual( 2, chunk.interpolants[0] );
+            self.assertEqual( 2, chunk.boundaries[0] );
+            self.assertEqual( 2, len( chunk.energies ) )
+            self.assertEqual( 2, len( chunk.cross_sections ) )
+            self.assertAlmostEqual( 10., chunk.energies[0] )
+            self.assertAlmostEqual( 1e+11, chunk.energies[1] )
+            self.assertAlmostEqual( 2.748960e+8, chunk.cross_sections[0] )
+            self.assertAlmostEqual( 1.31176E-5, chunk.cross_sections[1] )
 
-            self.assertEqual( 5, chunk.NC )
+            self.assertEqual( 4, chunk.NC )
 
             # verify string
             self.assertEqual( self.chunk + self.valid_SEND,
-                              chunk.to_string( 125, 23 ) )
+                              chunk.to_string( 100, 23 ) )
 
         # the data is given explicitly
-        chunk = Section( mt = 102,  zaid = 1001, awr = 0.9991673,
-                         epe = 2.224648e+6, efl = 3.224648e+6,
-                         interpolants = [ 5, 2 ], boundaries = [ 3, 6 ],
-                         energies = [ 1e-5, 2e-5, 7.5e+5,
-                                      1.9e+7, 1.95e+7, 2e+7 ],
-                         xs = [ 1.672869e+1, 1.182897e+1, 3.347392e-5,
-                                2.751761e-5, 2.731301e-5, 2.710792e-5 ] )
+        chunk = Section( mt = 525,  zaid = 1000, awr = 0.9992414,
+                         epe = 1., efl = 2.,
+                         interpolants = [ 2 ], boundaries = [ 2 ],
+                         energies = [ 10., 1e+11 ],
+                         xs = [ 2.748960e+8, 1.31176E-5 ] )
 
         verify_chunk( self, chunk )
 
@@ -86,7 +73,7 @@ class Test_ENDFtk_MF23_Section( unittest.TestCase ) :
         tape = Tape.from_string( self.valid_TPID + self.chunk +
                                  self.valid_SEND + self.valid_FEND +
                                  self.valid_MEND + self.valid_TEND )
-        chunk = tape.material( 125 ).file( 23 ).section( 102 ).parse()
+        chunk = tape.material( 100 ).file( 23 ).section( 525 ).parse()
 
         verify_chunk( self, chunk )
 
--- src/ENDFtk/section/23/test/23.test.cpp
+++ src/ENDFtk/section/23/test/23.test.cpp
@@ -22,17 +22,15 @@ SCENARIO( "section::Type< 23 >" ) {
 
     WHEN( "the data is given explicitly" ) {
 
-      int mt = 102;
-      int zaid = 1001;
-      double awr = 0.9991673;
-      double epe = 2.224648e+6;
-      double efl = 3.224648e+6;
-      std::vector< long > interpolants = { 5, 2 };
-      std::vector< long > boundaries = { 3, 6 };
-      std::vector< double > energies = { 1e-5, 2e-5, 7.5e+5,
-                                         1.9e+7, 1.95e+7, 2e+7 };
-      std::vector< double > xs = { 1.672869e+1, 1.182897e+1, 3.347392e-5,
-                                   2.751761e-5, 2.731301e-5, 2.710792e-5 };
+      int mt = 525;
+      int zaid = 1000;
+      double awr = 0.9992414;
+      double epe = 1.;
+      double efl = 2.;
+      std::vector< long > interpolants = { 2 };
+      std::vector< long > boundaries = { 2 };
+      std::vector< double > energies = { 10., 1e+11 };
+      std::vector< double > xs = { 274896000., 1.31176E-5 };
 
       section::Type< 23 > chunk( mt, zaid, awr, epe, efl,
                                 std::move( boundaries ),
@@ -49,7 +47,7 @@ SCENARIO( "section::Type< 23 >" ) {
 
         std::string buffer;
         auto output = std::back_inserter( buffer );
-        chunk.print( output, 125, 23 );
+        chunk.print( output, 100, 23 );
 
         CHECK( buffer == sectionString );
       } // THEN
@@ -62,7 +60,7 @@ SCENARIO( "section::Type< 23 >" ) {
       long lineNumber = 0;
       HeadRecord head( begin, end, lineNumber );
 
-      section::Type< 23 > chunk( head, begin, end, lineNumber, 125 );
+      section::Type< 23 > chunk( head, begin, end, lineNumber, 100 );
 
       THEN( "a section::Type< 23 > can be constructed and "
             "members can be tested" ) {
@@ -74,7 +72,7 @@ SCENARIO( "section::Type< 23 >" ) {
 
         std::string buffer;
         auto output = std::back_inserter( buffer );
-        chunk.print( output, 125, 23 );
+        chunk.print( output, 100, 23 );
 
         CHECK( buffer == sectionString );
       } // THEN
@@ -82,7 +80,7 @@ SCENARIO( "section::Type< 23 >" ) {
 
     WHEN( "there is a tree::Section" ) {
 
-      tree::Section section( 125, 23, 102, std::string( sectionString ) );
+      tree::Section section( 125, 23, 525, std::string( sectionString ) );
 
       section::Type< 23 > chunk = section.parse< 23 >();
 
@@ -96,7 +94,7 @@ SCENARIO( "section::Type< 23 >" ) {
 
         std::string buffer;
         auto output = std::back_inserter( buffer );
-        chunk.print( output, 125, 23 );
+        chunk.print( output, 100, 23 );
 
         CHECK( buffer == sectionString );
       } // THEN
@@ -117,64 +115,54 @@ SCENARIO( "section::Type< 23 >" ) {
       THEN( "an exception is thrown" ){
 
         CHECK_THROWS( section::Type< 23 >( head, begin, end,
-                                            lineNumber, 125 ) );
+                                            lineNumber, 100 ) );
       } // THEN
     } // WHEN
   } // THEN
 } // SCENARIO
 
-std::string chunk(){
+std::string chunk() {
+  
   return
-    " 1.001000+3 9.991673-1          0          0          0          0 12523102     \n"
-    " 2.224648+6 3.224648+6          0          0          2          6 12523102     \n"
-    "          3          5          6          2                       12523102     \n"
-    " 1.000000-5 1.672869+1 2.000000-5 1.182897+1 7.500000+5 3.347392-5 12523102     \n"
-    " 1.900000+7 2.751761-5 1.950000+7 2.731301-5 2.000000+7 2.710792-5 12523102     \n";
+    " 1.000000+3 9.992414-1          0          0          0          0 10023525     \n"
+    " 1.000000+0 2.000000+0          0          0          1          2 10023525     \n"
+    "          2          2                                             10023525     \n"
+    " 1.000000+1 2.748960+8 1.00000+11 1.311760-5                       10023525     \n";
 }
 
 void verifyChunk( const section::Type< 23 >& chunk ) {
 
-  CHECK( 102 == chunk.MT() );
-  CHECK( 1001 == chunk.ZA() );
-  CHECK( 0.9991673 == Approx( chunk.AWR() ) );
-  CHECK( 0.9991673 == Approx( chunk.atomicWeightRatio() ) );
-  CHECK( 2.224648e+6 == Approx( chunk.EPE() ) );
-  CHECK( 2.224648e+6 == Approx( chunk.subshellBindingEnergy() ) );
-  CHECK( 3.224648e+6 == Approx( chunk.EFL() ) );
-  CHECK( 3.224648e+6 == Approx( chunk.fluorescenceYield() ) );
+  CHECK( 525 == chunk.MT() );
+  CHECK( 1000 == chunk.ZA() );
+  CHECK( 0.9992414 == Approx( chunk.AWR() ) );
+  CHECK( 0.9992414 == Approx( chunk.atomicWeightRatio() ) );
+  CHECK( 1. == Approx( chunk.EPE() ) );
+  CHECK( 1. == Approx( chunk.subshellBindingEnergy() ) );
+  CHECK( 2. == Approx( chunk.EFL() ) );
+  CHECK( 2. == Approx( chunk.fluorescenceYield() ) );
 
-  CHECK( 6 == chunk.NP() );
-  CHECK( 2 == chunk.NR() );
-  CHECK( 2 == chunk.interpolants().size() );
-  CHECK( 2 == chunk.boundaries().size() );
-  CHECK( 5 == chunk.interpolants()[0] );
-  CHECK( 2 == chunk.interpolants()[1] );
-  CHECK( 3 == chunk.boundaries()[0] );
-  CHECK( 6 == chunk.boundaries()[1] );
-  CHECK( 6 == chunk.energies().size() );
-  CHECK( 6 == chunk.crossSections().size() );
-  CHECK( 1e-5 == Approx( chunk.energies()[0] ) );
-  CHECK( 2e-5 == Approx( chunk.energies()[1] ) );
-  CHECK( 7.5e+5 == Approx( chunk.energies()[2] ) );
-  CHECK( 1.9e+7 == Approx( chunk.energies()[3] ) );
-  CHECK( 1.95e+7 == Approx( chunk.energies()[4] ) );
-  CHECK( 2e+7 == Approx( chunk.energies()[5] ) );
-  CHECK( 1.672869e+1 == Approx( chunk.crossSections()[0] ) );
-  CHECK( 1.182897e+1 == Approx( chunk.crossSections()[1] ) );
-  CHECK( 3.347392e-5 == Approx( chunk.crossSections()[2] ) );
-  CHECK( 2.751761e-5 == Approx( chunk.crossSections()[3] ) );
-  CHECK( 2.731301e-5 == Approx( chunk.crossSections()[4] ) );
-  CHECK( 2.710792e-5 == Approx( chunk.crossSections()[5] ) );
+  CHECK( 2 == chunk.NP() );
+  CHECK( 1 == chunk.NR() );
+  CHECK( 1 == chunk.interpolants().size() );
+  CHECK( 1 == chunk.boundaries().size() );
+  CHECK( 2 == chunk.interpolants()[0] );
+  CHECK( 2 == chunk.boundaries()[0] );
+  CHECK( 2 == chunk.energies().size() );
+  CHECK( 2 == chunk.crossSections().size() );
+  CHECK( 10. == Approx( chunk.energies()[0] ) );
+  CHECK( 1e+11 == Approx( chunk.energies()[1] ) );
+  CHECK( 2.748960e+8 == Approx( chunk.crossSections()[0] ) );
+  CHECK( 1.31176E-5 == Approx( chunk.crossSections()[1] ) );
 
-  CHECK( 5 == chunk.NC() );
+  CHECK( 4 == chunk.NC() );
 }
 
 std::string validSEND(){
   return
-    "                                                                   12523  0     \n";
+    "                                                                   10023  0     \n";
 }
 
 std::string invalidSEND(){
   return
-    "                                                                   12523  1     \n";
+    "                                                                   10023  1     \n";
 }
