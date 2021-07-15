e93eab652e9423e921048c8c1df47856fb7eacf5
Wim Haeck <whaeck@gmail.com>
2021-07-15 09:44:20 -0600

    Adding MF27

---
+++ src/ENDFtk/section/27/src/ctor.hpp
@@ -0,0 +1,47 @@
+/**
+ *  @brief Constructor
+ *
+ *  @param[in] mt             the MT number
+ *  @param[in] zaid           the ZA  identifier
+ *  @param[in] awr            the atomic mass ratio
+ *  @param[in] boundaries     the interpolation range boundaries
+ *  @param[in] interpolants   the interpolation types for each range
+ *  @param[in] energies       the energy values
+ *  @param[in] values         the form factor or scattering function values
+ */
+Type( int mt, double zaid, double awr,
+      std::vector< long >&& boundaries, std::vector< long >&& interpolants,
+      std::vector< double >&& energies, std::vector< double >&& values ) :
+  Base( zaid, awr, mt ),
+  table( 0.0, static_cast< int >( zaid ) / 1000, 0, 0,
+         std::move( boundaries ), std::move( interpolants ),
+         std::move( energies ), std::move( values ) ) {}
+
+/**
+ *  @brief Constructor (from a buffer)
+ *
+ *  @tparam Iterator        a buffer iterator
+ *
+ *  @param[in] head         the head record of the section
+ *  @param[in] it           the current position in the buffer
+ *  @param[in] end          the end of the buffer
+ *  @param[in] lineNumber   the current line number
+ *  @param[in] MAT          the expected MAT number
+ */
+template< typename Iterator >
+Type ( HEAD& head,
+       Iterator& begin,
+       const Iterator& end,
+       long& lineNumber,
+       int MAT )
+  try: Base( head, MAT, 27 ),
+       table( begin, end, lineNumber, head.MAT(), 27, head.MT() ){
+    readSEND(begin, end, lineNumber, MAT, 27 );
+  }
+  catch( std::exception& e ) {
+
+    Log::info
+    ( "Encountered error while reading section {} of file 27 of material {}",
+      head.MT(), MAT );
+    throw e;
+  }
