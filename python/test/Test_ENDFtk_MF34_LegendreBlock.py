069dc345c4320ca17cc0265ed5ad64d8c3f3be56
Nathan Gibson <ngibson@lanl.gov>
2021-08-06 14:37:51 -0600

    Added Python tests.

---
+++ python/test/Test_ENDFtk_MF34_LegendreBlock.py
@@ -0,0 +1,100 @@
+# standard imports
+import unittest
+
+# third party imports
+
+# local imports
+from ENDFtk.MF34 import LegendreBlock
+from ENDFtk.MF34 import SquareMatrix
+from ENDFtk.MF34 import RectangularMatrix
+from ENDFtk.MF34 import CovariancePairs
+
+class Test_ENDFtk_MF34_LegendreBlock( unittest.TestCase ) :
+    """Unit test for the LegendreBlock class."""
+
+    chunk = (
+        " 0.000000+0 0.000000+0          1          2          3          3943734  2     \n"
+        " 0.000000+0 0.000000+0          0          5          7          3943734  2     \n"
+        " 0.000000+0 1.000000+2 2.000000+2 1.000000+0 2.000000+0 3.000000+0943734  2     \n"
+        " 4.000000+0                                                       943734  2     \n"
+        " 0.000000+0 0.000000+0          0          6         13          3943734  2     \n"
+        " 1.100000+0 1.200000+0 1.300000+0 2.100000+0 2.200000+0 2.300000+0943734  2     \n"
+        " 2.400000+0 1.000000+0 2.000000+0 3.000000+0 4.000000+0 5.000000+0943734  2     \n"
+        " 6.000000+0                                                       943734  2     \n"
+        " 0.000000+0 0.000000+0          0          1          4          2943734  2     \n"
+        " 1.000000+0 2.000000+0 3.000000+0 4.000000+0                      943734  2     \n" )
+
+    def test_component( self ) :
+
+        def verify_chunk( self, chunk ) :
+
+            # LegendreBlock CONT record
+            self.assertEqual( 1, chunk.L )
+            self.assertEqual( 1, chunk.first_legendre_order )
+            self.assertEqual( 2, chunk.L1 )
+            self.assertEqual( 2, chunk.second_legendre_order )
+            self.assertEqual( 3, chunk.LCT )
+            self.assertEqual( 3, chunk.coordinate_system )
+            self.assertEqual( 3, chunk.NI )
+            self.assertEqual( 3, chunk.number_data_blocks )
+            self.assertEqual( 3, len(chunk.data) )
+
+            stuff = chunk.data[0]
+
+            # metadata
+            self.assertEqual( 0, stuff.LS )
+            self.assertEqual( 0, stuff.symmetry_flag )
+            self.assertFalse( stuff.is_symmetric )
+            self.assertEqual( 5, stuff.LB )
+            self.assertEqual( 5, stuff.procedure )
+            self.assertEqual( 7, stuff.NT )
+            self.assertEqual( 7, stuff.number_values )
+            self.assertEqual( 3, stuff.NE )
+            self.assertEqual( 3, stuff.number_energies )
+
+            # values
+            self.assertAlmostEqual( 0., stuff.energies[0] )
+            self.assertAlmostEqual( 100., stuff.energies[1] )
+            self.assertAlmostEqual( 200., stuff.energies[2] )
+            self.assertAlmostEqual( 1., stuff.values[0] )
+            self.assertAlmostEqual( 2., stuff.values[1] )
+            self.assertAlmostEqual( 3., stuff.values[2] )
+            self.assertAlmostEqual( 4., stuff.values[3] )
+
+            self.assertEqual( 10, chunk.NC )
+
+            # verify string
+            self.assertEqual( self.chunk,
+                              chunk.to_string( 9437, 34, 2 ) )
+
+        # the data is given explicitly for MT==MT1
+        row_energies = [ 1.1, 1.2, 1.3 ]
+        column_energies = [ 2.1, 2.2, 2.3, 2.4 ]
+        values = [ 1, 2, 3, 4, 5, 6 ]
+        energies = [ 0, 100, 200 ]
+        svalues = [ 1, 2, 3, 4 ]
+
+        ni = [
+            SquareMatrix( 0, energies, svalues ),
+            RectangularMatrix( row_energies, column_energies, values ),
+            CovariancePairs( 1, [1., 3.], [2., 4.] )
+            ]
+
+        chunk = LegendreBlock( order=1, order1=2, lct=3, data=ni )
+
+        verify_chunk( self, chunk )
+
+        # the data is read from a string
+        chunk = LegendreBlock.from_string( self.chunk, 9437, 34, 2 )
+
+        verify_chunk( self, chunk )
+
+        # the data is copied
+        copy = LegendreBlock( chunk )
+
+        verify_chunk( self, copy )
+
+
+if __name__ == '__main__' :
+
+    unittest.main()
---
+++ python/test/Test_ENDFtk_MF34_Section.py
@@ -0,0 +1,108 @@
+# standard imports
+import unittest
+
+# third party imports
+
+# local imports
+from ENDFtk.MF34 import Section
+from ENDFtk.MF34 import ReactionBlock
+from ENDFtk.MF34 import LegendreBlock
+from ENDFtk.MF34 import SquareMatrix
+from ENDFtk.tree import Tape
+
+class Test_ENDFtk_MF34_Section( unittest.TestCase ) :
+    """Unit test for the Section class."""
+
+    chunk = (
+        " 9.423900+4 2.369986+2          0          1          0          1943734  2     \n"
+        " 0.000000+0 0.000000+0          0         51          1          1943734  2     \n"
+        " 0.000000+0 0.000000+0          1          1          0          1943734  2     \n"
+        " 0.000000+0 0.000000+0          0          5          7          3943734  2     \n"
+        " 0.000000+0 1.000000+2 2.000000+2 1.000000+0 2.000000+0 3.000000+0943734  2     \n"
+        " 4.000000+0                                                       943734  2     \n" )
+
+    valid_TPID = 'Just a tape identifier                                                          \n'
+    valid_SEND = '                                                                  943734  0     \n'
+    valid_FEND = '                                                                  9437 0  0     \n'
+    valid_MEND = '                                                                     0 0  0     \n'
+    valid_TEND = '                                                                    -1 0  0     \n'
+    invalid_SEND = '                                                                  943734  1     \n'
+
+    def test_section( self ) :
+
+        def verify_chunk( self, chunk ) :
+
+            # HEAD record
+            self.assertAlmostEqual( 94239.,  chunk.ZA )
+            self.assertAlmostEqual( 2.369986e+2,  chunk.AWR )
+            self.assertAlmostEqual( 2.369986e+2,  chunk.atomic_weight_ratio )
+            self.assertEqual( 1, chunk.LTT )
+            self.assertEqual( 1, chunk.representation )
+
+            sub = chunk.reactions[0]
+
+            # subsection CONT record
+            self.assertEqual( 0, sub.MAT1 )
+            self.assertEqual( 0, sub.second_material_number )
+            self.assertEqual( 51, sub.MT1 )
+            self.assertEqual( 51, sub.second_section_number )
+            self.assertEqual( 1, sub.NL )
+            self.assertEqual( 1, sub.first_number_legendre )
+            self.assertEqual( 1, sub.NL1 )
+            self.assertEqual( 1, sub.second_number_legendre )
+            self.assertEqual( 1, sub.NSS )
+            self.assertEqual( 1, sub.number_legendre_blocks )
+            self.assertEqual( 1, len(sub.legendre_blocks) )
+
+            self.assertEqual( 6, chunk.NC )
+
+            # verify string
+            self.assertEqual( self.chunk + self.valid_SEND,
+                              chunk.to_string( 9437, 34 ) )
+
+        # the data is given explicitly
+        chunk = Section(
+            mt=2, zaid=94239, awr=2.369986e+2, ltt=1,
+            reactions=[ReactionBlock(
+                2, 51, 1, 1,
+                [LegendreBlock(
+                    1, 1, 0,
+                    [SquareMatrix(
+                        0, [0, 100, 200], [1, 2, 3, 4]
+                        )]
+                    )]
+                )]
+            )
+
+        verify_chunk( self, chunk )
+
+        # the data is read from a string
+        chunk = Section.from_string( self.chunk + self.valid_SEND )
+
+        verify_chunk( self, chunk )
+
+        # the data is retrieved from a tree element and parsed
+        tape = Tape.from_string( self.valid_TPID + self.chunk +
+                                 self.valid_SEND + self.valid_FEND +
+                                 self.valid_MEND + self.valid_TEND )
+        chunk = tape.material( 9437 ).file( 34 ).section( 2 ).parse()
+
+        verify_chunk( self, chunk )
+
+        # the data is copied
+        copy = Section( chunk )
+
+        verify_chunk( self, copy )
+
+    def test_failures( self ) :
+
+        print( '\n' )
+
+        # illegal SEND
+        with self.assertRaises( Exception ) :
+
+            chunk = Section.from_string( self.chunk + self.invalid_SEND )
+
+if __name__ == '__main__' :
+
+    unittest.main()
