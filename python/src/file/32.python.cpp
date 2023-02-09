3a33559243d6a9ba2ff60f6530c940f361ae466c
Wim Haeck <whaeck@gmail.com>
2023-02-09 10:51:01 -0700

    Added test for LCOMP=2, MultiLevelBreitWigner

--- python/src/file/32.python.cpp
+++ python/src/file/32.python.cpp
@@ -20,7 +20,7 @@ void wrapSection_32_151( python::module&, python::module& );
 void wrapFile_32( python::module& module, python::module& viewmodule ) {
 
   // type aliases
-//  using File = njoy::ENDFtk::file::Type< 3 >;
+  using File = njoy::ENDFtk::file::Type< 32 >;
   using MF32MT151 = std::reference_wrapper< const njoy::ENDFtk::section::Type< 32, 151 > >;
 
   // wrap views created by this file
@@ -36,51 +36,50 @@ void wrapFile_32( python::module& module, python::module& viewmodule ) {
   wrapSection_32_151( submodule, viewmodule );
 
   // create the file
-//  python::class_< File > file(
-//
-//    submodule,
-//    "File",
-//    "MF2 file - resonance parameters"
-//  );
+  python::class_< File > file(
+
+    submodule,
+    "File",
+    "MF2 file - resonance parameters"
+  );
 
   // common lambda
-//  auto get_section =
-//  [] ( const File& self, int mt ) -> std::variant< MF2MT151, MF2MT152 > {
-//
-//    switch ( mt ) {
-//
-//      case 151 : return self.section( 151_c );
-//      case 152 : return self.section( 152_c );
-//      default: throw std::runtime_error(
-//                    "Requested section number (" + std::to_string( mt ) +
-//                    ") does not correspond to a stored section" );
-//    }
-//  };
+  auto get_section =
+  [] ( const File& self, int mt ) -> MF32MT151 {
 
-//  file
-//  .def(
-//
-//    "section",
-//    get_section,
-//    python::arg( "mt" ),
-//    "Return the section with the requested MT number\n\n"
-//    "Arguments:\n"
-//    "    self    the file\n"
-//    "    mt      the MT number of the section to be returned",
-//    python::return_value_policy::reference_internal
-//  )
-//  .def(
-//
-//    "MT",
-//    get_section,
-//    python::arg( "mt" ),
-//    "Return the section with the requested MT number\n\n"
-//    "Arguments:\n"
-//    "    self    the file\n"
-//    "    mt      the MT number of the section to be returned",
-//    python::return_value_policy::reference_internal
-//  );
-//
-//  // add standard file definitions
-//  addStandardFileDefinitions< File >( file );
+    switch ( mt ) {
+
+      case 151 : return self.section( 151_c );
+      default: throw std::runtime_error(
+                    "Requested section number (" + std::to_string( mt ) +
+                    ") does not correspond to a stored section" );
+    }
+  };
+
+  file
+  .def(
+
+    "section",
+    get_section,
+    python::arg( "mt" ),
+    "Return the section with the requested MT number\n\n"
+    "Arguments:\n"
+    "    self    the file\n"
+    "    mt      the MT number of the section to be returned",
+    python::return_value_policy::reference_internal
+  )
+  .def(
+
+    "MT",
+    get_section,
+    python::arg( "mt" ),
+    "Return the section with the requested MT number\n\n"
+    "Arguments:\n"
+    "    self    the file\n"
+    "    mt      the MT number of the section to be returned",
+    python::return_value_policy::reference_internal
+  );
+
+  // add standard file definitions
+  addStandardFileDefinitions< File >( file );
 }
--- src/ENDFtk/section/32/151/CompactCorrelationMatrix/src/compact.hpp
+++ src/ENDFtk/section/32/151/CompactCorrelationMatrix/src/compact.hpp
@@ -3,69 +3,77 @@ std::vector< std::tuple< unsigned int, unsigned int,
 
   // this code is not pretty, but the INTG records are just dumb so ...
 
-  // the maximum number of values per line
-  unsigned int n = 0;
-  switch ( this->NDIGIT() ) {
-
-    case 2: { n = 18; break; }
-    case 3: { n = 13; break; }
-    case 4: { n = 11; break; }
-    case 5: { n =  9; break; }
-    case 6: { n =  8; break; }
-    default: { /* unreachable */ break; }
-  }
-
-  // conversion factor
-  double factor = 1;
-  switch ( this->NDIGIT() ) {
-
-    case 2: { factor = 100; break; }
-    case 3: { factor = 1000; break; }
-    case 4: { factor = 10000; break; }
-    case 5: { factor = 100000; break; }
-    case 6: { factor = 1000000; break; }
-    default: { /* unreachable */ break; }
-  }
-
-  // conversion function
-  auto convert = [factor] ( double value ) -> int {
-
-    value *= factor;
-    if ( std::abs( value ) <= 1. ) {
-
-      return 0;
-    }
-    if ( value > 0. ) {
-
-      return std::floor( value );
-    }
-    else {
-
-      return -std::floor( value );
-    }
-  };
-
-  // compact the correlation matrix
   std::vector< std::tuple< unsigned int, unsigned int,
-                           std::vector< int > > > lines = {
+                           std::vector< int > > > lines;
+  if ( this->correlations_.size() != 0 ) {
 
-    { this->i_[0], this->j_[0], { convert( this->correlations_[0] ) } }
-  };
-  unsigned int size = this->i_.size();
-  for ( unsigned int i = 1; i < size; ++i ) {
+    // the maximum number of values per line
+    unsigned int n = 0;
+    switch ( this->NDIGIT() ) {
 
-    if ( this->i_[i] == std::get< 0 >( lines.back() ) ) {
+      case 2: { n = 18; break; }
+      case 3: { n = 13; break; }
+      case 4: { n = 11; break; }
+      case 5: { n =  9; break; }
+      case 6: { n =  8; break; }
+      default: { /* unreachable */ break; }
+    }
 
-      if ( this->j_[i] < std::get< 1 >( lines.back() ) + std::get< 2 >( lines.back() ).size() - 1 + n ) {
+    // conversion factor
+    double factor = 1;
+    switch ( this->NDIGIT() ) {
 
-        int pad = this->j_[i] - std::get< 1 >( lines.back() ) - std::get< 2 >( lines.back() ).size();
-        if ( pad > 0 ) {
+      case 2: { factor = 100; break; }
+      case 3: { factor = 1000; break; }
+      case 4: { factor = 10000; break; }
+      case 5: { factor = 100000; break; }
+      case 6: { factor = 1000000; break; }
+      default: { /* unreachable */ break; }
+    }
 
-          std::get< 2 >( lines.back() ).insert( std::get< 2 >( lines.back() ).end(),
-                                                pad,
-                                                0 );
+    // conversion function
+    auto convert = [factor] ( double value ) -> int {
+
+      value *= factor;
+      if ( std::abs( value ) <= 1. ) {
+
+        return 0;
+      }
+      if ( value > 0. ) {
+
+        return std::floor( value );
+      }
+      else {
+
+        return -std::floor( value );
+      }
+    };
+
+    // compact the correlation matrix
+    lines.push_back( { this->i_[0], this->j_[0], { convert( this->correlations_[0] ) } } );
+    unsigned int size = this->i_.size();
+    for ( unsigned int i = 1; i < size; ++i ) {
+
+      if ( this->i_[i] == std::get< 0 >( lines.back() ) ) {
+
+        if ( this->j_[i] < std::get< 1 >( lines.back() ) + std::get< 2 >( lines.back() ).size() - 1 + n ) {
+
+          int pad = this->j_[i] - std::get< 1 >( lines.back() ) - std::get< 2 >( lines.back() ).size();
+          if ( pad > 0 ) {
+
+            std::get< 2 >( lines.back() ).insert( std::get< 2 >( lines.back() ).end(),
+                                                  pad,
+                                                  0 );
+          }
+          std::get< 2 >( lines.back() ).emplace_back( convert( this->correlations_[i] ) );
         }
-        std::get< 2 >( lines.back() ).emplace_back( convert( this->correlations_[i] ) );
+        else {
+
+          lines.emplace_back( std::make_tuple (
+                                this->i_[i],
+                                this->j_[i],
+                                std::vector< int >{ convert( this->correlations_[i] ) } ) );
+        }
       }
       else {
 
@@ -74,13 +82,6 @@ std::vector< std::tuple< unsigned int, unsigned int,
                               this->j_[i],
                               std::vector< int >{ convert( this->correlations_[i] ) } ) );
       }
-    }
-    else {
-
-      lines.emplace_back( std::make_tuple (
-                            this->i_[i],
-                            this->j_[i],
-                            std::vector< int >{ convert( this->correlations_[i] ) } ) );
     }
   }
 
