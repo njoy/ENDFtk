caa728e912f9be825a2ce98388bcb3d7fcd3c473
Wim Haeck <whaeck@gmail.com>
2023-03-14 11:15:38 -0600

    More changes

--- src/ENDFtk/section/26/DiscreteTwoBodyScattering.hpp
+++ src/ENDFtk/section/26/DiscreteTwoBodyScattering.hpp
@@ -2,10 +2,7 @@
  *  @class
  *  @brief Discrete two-body scattering data for secondary particles (LAW=2)
  *
- *  The DiscreteTwoBodyScattering class is used to represent the discrete
- *  two-body scattering law=2 data of MF6.
- *
- *  The DiscreteTwoBodyScattering class is used to represent the two-body angular
+\ *  The DiscreteTwoBodyScattering class is used to represent the two-body angular
  *  distribution law=2 data of MF26. It is similar to the MF6 law=2, but only
  *  supports the LANG=11 to 15 options.
  *
