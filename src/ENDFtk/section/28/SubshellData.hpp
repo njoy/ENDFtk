b077cf2b462298ea79f01a19b750b48578a886d6
Wim Haeck <whaeck@gmail.com>
2024-09-07 04:57:14 -0600

    More updates for atomic relaxation

--- src/ENDFtk/section/28/SubshellData.hpp
+++ src/ENDFtk/section/28/SubshellData.hpp
@@ -28,7 +28,10 @@ public:
   /**
    *  @brief Return the subshell designator
    */
-  unsigned int SUBI() const { return ListRecord::C1(); }
+  unsigned int SUBI() const {
+
+    return static_cast< unsigned int >( std::round( ListRecord::C1() ) );
+  }
 
   /**
    *  @brief Return the subshell designator
@@ -58,12 +61,12 @@ public:
   /**
    *  @brief Return the number of electrons in the subshell
    */
-  unsigned int ELN() const { return ListRecord::list()[1]; }
+  double ELN() const { return ListRecord::list()[1]; }
 
   /**
    *  @brief Return the number of electrons in the subshell
    */
-  unsigned int numberSubshellElectrons() const { return this->ELN(); }
+  double numberSubshellElectrons() const { return this->ELN(); }
 
   /**
    *  @brief Return the secondary subshell designators (one for each transition)
