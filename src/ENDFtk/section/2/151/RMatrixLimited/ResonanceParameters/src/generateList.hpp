84113fb8b87a9a800e8223f49bd2fe105e927c5c
Wim Haeck <whaeck@gmail.com>
2026-01-22 13:26:40 -0700

    allowing for an empty spin group

--- src/ENDFtk/section/2/151/RMatrixLimited/ResonanceParameters/src/generateList.hpp
+++ src/ENDFtk/section/2/151/RMatrixLimited/ResonanceParameters/src/generateList.hpp
@@ -3,6 +3,7 @@ generateList( std::vector< double >&& energies,
               std::vector< std::vector< double > >&& parameters ) {
 
   unsigned int nrs = energies.size();
+  unsigned int nx = 1;
   if ( parameters.size() != nrs ) {
 
     Log::info( "The number of resonance energies and resonance parameters is "
@@ -13,33 +14,33 @@ generateList( std::vector< double >&& energies,
     throw std::exception();
   }
 
+  std::vector< double > list;
   if ( nrs == 0 ) {
 
-    Log::info( "No resonances are given" );
-    Log::info( "energies.size(): {}", energies.size() );
-    Log::info( "parameters.size(): {}", parameters.size() );
-    throw std::exception();
+    list = { 0., 0., 0., 0., 0., 0. };
   }
+  else {
 
-  unsigned int nch = parameters.front().size();
-  unsigned int padding = ( 6 - ( ( 1 + nch ) % 6 ) ) % 6;
-  unsigned int nx = nrs * ( 1 + nch + padding ) / 6;
+    unsigned int nch = parameters.front().size();
+    unsigned int padding = ( 6 - ( ( 1 + nch ) % 6 ) ) % 6;
+    nx = nrs * ( 1 + nch + padding ) / 6;
 
-  std::vector< double > list;
-  for ( unsigned int i = 0; i < nrs; ++i ) {
+    for ( unsigned int i = 0; i < nrs; ++i ) {
 
-    if ( parameters[i].size() != nch ) {
+      if ( parameters[i].size() != nch ) {
 
-      Log::info( "The number of channels for the resonance energy with index {}"
-                 "is not equal to the expected value", i );
-      Log::info( "NCH value: {}", nch );
-      Log::info( "parameters[i].size(): {}", parameters[i].size() );
-      throw std::exception();
+        Log::info( "The number of channels for the resonance energy with index {}"
+                   "is not equal to the expected value", i );
+        Log::info( "NCH value: {}", nch );
+        Log::info( "parameters[i].size(): {}", parameters[i].size() );
+        throw std::exception();
+      }
+
+      list.push_back( energies[i] );
+      list.insert( list.end(), parameters[i].begin(), parameters[i].end() );
+      list.insert( list.end(), padding, 0.0 );
     }
-
-    list.push_back( energies[i] );
-    list.insert( list.end(), parameters[i].begin(), parameters[i].end() );
-    list.insert( list.end(), padding, 0.0 );
   }
+
   return ListRecord( 0.0, 0.0, 0, nrs, nx, std::move( list ) );
 }
